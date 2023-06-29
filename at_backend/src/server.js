0644a303862e50d365ae635ec1fdd1fd6b08b293
sanju <sanjaykumarmullu333@gmail.com>
2023-06-29 22:37:14 +0530

    ok9

--- at_backend/src/server.js
+++ at_backend/src/server.js
@@ -6,15 +6,19 @@ app.use(cors())
 app.use(express.json())
 app.get('/',(req,res)=>{
     res.send("server is running successfully!");
-});
+});+
 // app.get('/inter',(req,res)=>{
 //     const data=db.collection('signup').insertOne({name:"sanju"});
 //     res.json(data);
 // })
-// app.get('/install',(req,res)=>{
-//     const push=db.collection('register').insertOne({name:"sanju"});
-//     res.send(push);
-// })
+app.get('/install',(req,res)=>{
+    const push=db.collection('register').insertOne({name:"sanju"});
+    res.json(push);
+})
+app.post('/signup/:name/:regd/:year/:section/:email/:phno',async(req,res)=>{
+    const details1=await db.collection('signup').insertOne({name:req.params.name,regd:req.params.regd,year:req.params.year,section:req.params.section,email:req.params.email,phno:req.params.phno});
+    res.json(details1);
+})
 app.get('/check/:name',async(req,res)=>{
     const details=await db.collection('signup').findOne({name:req.params.name});
     res.json(details);
@@ -26,10 +30,6 @@ app.get('/check1/:email',async(req,res)=>{
 app.get('/check2/:phno',async(req,res)=>{
     const details=await db.collection('signup').findOne({phno:req.params.phno});
     res.json(details);
-})
-app.post('/signup/:name/:regd/:year/:section/:email/:phno',async(req,res)=>{
-    const details1=await db.collection('signup').insertOne({name:req.params.name,regd:req.params.regd,year:req.params.year,section:req.params.section,email:req.params.email,phno:req.params.phno});
-    res.json(details1);
 })
 connectToDB(()=>{
     app.listen(6000,()=>{
