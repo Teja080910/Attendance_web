2146dca17c826f0806047ed8249cd7824394a4a3
AST <tejasimma36@gmail.com>
2023-07-12 21:04:18 +0530

    ok8

--- at_frontend/src/collections/book/sdisplay.js
+++ at_frontend/src/collections/book/sdisplay.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios"; 
-const regd=localStorage.regd;
 const Sdisplay= () => {
+    const[regd,Setregd]=useState("");
     const[data,setData]=useState([]);
+    const Search =async()=>{
     useEffect(()=>{
         axios.get('http://localhost:4000/detail/'+regd)
         .then((responce)=>{   
@@ -12,9 +13,15 @@ const Sdisplay= () => {
             console.log(err);
         }
     )},[])
+        window.location.reload(false);
+    }
 
     return(
         <>
+         <div className="search">
+        <br/> <input type="text" className="width" id="regd" name="reg" placeholder="Register Number" onChange={(e)=>Setregd(e.target.value)}/>
+        <button className="height" onclick={Search}>search</button>
+        </div>
         {
             data.map((item)=>{ 
                 <table className="table1">
