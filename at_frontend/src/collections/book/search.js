8cd4d48743386bcfb700b42b66998454b0f1a57d
sanju <sanjaykumarmullu333@gmail.com>
2023-07-14 12:17:08 +0530

    ok333

--- at_frontend/src/collections/book/search.js
+++ at_frontend/src/collections/book/search.js
@@ -1,15 +1,16 @@
 import React , {useState} from "react";
 import axios from "axios";
 const Search=()=>{
-    const[regd,Setregd]=useState("");
+    const[regd,Setregd]=useState(""); 
     localStorage.regd=regd;
-    const search3 =async()=>{
+    const Search3 =async()=>{
         const  responce3 = await axios.get("http://localhost:4000/find1/"+regd);
         if(responce3.data)
         {
              window.location.reload(false);
         }
-        else{
+        else
+        {
             alert("not registered");
         }
     }
@@ -17,7 +18,7 @@ const Search=()=>{
     <>
     <div className="search">
     <br/> <input type="text" className="width" id="regd" name="regd" placeholder="Register Number" onChange={(e)=>Setregd(e.target.value)}/>
-    <button className="height" onclick={search3}>search</button>
+    <button className="height" onclick={Search3}>search</button>
     </div>
     </>
     )
